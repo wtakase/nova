853dc981181005c43184436ad27e855369ae3ef8
Belmiro Moreira <moreira.belmiro.email.lists@gmail.com>
2014-09-15 00:21:53 +0200

    CERN - Remove capability support on target_cell_project
    The cell filter now relies on config params.
    Also, supports project scheduling in a shared and private cells.

--- nova/cells/filters/target_cell_project.py
+++ nova/cells/filters/target_cell_project.py
@@ -17,7 +17,21 @@
 Project target cell filter.
 """
 
+from oslo.config import cfg
 from nova.cells import filters
+
+cell_project_target_cell_opts = [
+        cfg.ListOpt('cells_default',
+               default=[],
+               help='list of default cells'),
+        cfg.DictOpt('cells_projects',
+               default={},
+               help='list of cells and projects')
+]
+
+
+CONF = cfg.CONF
+CONF.register_opts(cell_project_target_cell_opts, group='cells')
 
 class TargetCellProjectFilter(filters.BaseCellFilter):
     """Target Cell Project Filter"""
@@ -30,19 +44,26 @@ class TargetCellProjectFilter(filters.BaseCellFilter):
         instance_properties = request_spec['instance_properties']
         instance_project_id = instance_properties['project_id']
 
+        cells = list(cells)
         project_cells = []
-        generic_cells = []
+        cells_projects = CONF.cells.cells_projects
+        cells_default = CONF.cells.cells_default
 
-        for cell in cells:
-            projects = cell.capabilities.get('projects')
-            if projects != None:
+        scheduler = filter_properties['scheduler']
+        if len(cells) == 1 and\
+           cells[0].name == scheduler.state_manager.get_my_state().name:
+            return cells
+
+        generic_cells = [x for x in cells if x.name in cells_default]
+
+        for cell in cells_projects.keys():
+            projects = [x.strip() for x in cells_projects[cell].split(';')]
+            if projects != []:
                 if instance_project_id in projects:
                     project_cells.append(cell)
-            else:
-                generic_cells.append(cell)
 
         if project_cells != []:
-            return project_cells
+            return [x for x in cells if x.name in project_cells]
 
         return generic_cells
 
